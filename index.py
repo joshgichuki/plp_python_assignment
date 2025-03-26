8c30834fefdbb93f15949bbbfeb74fa4c8c01e16
joshgichuki <gichukijoshua10@gmail.com>
2025-03-26 13:52:25 +0300

    Add Python assignment solution

--- index.py
+++ index.py
@@ -1,1 +1,39 @@
-print("hello world");
+#print("hello world")
+
+
+#age=27
+#print (age)
+#list
+#languages = ["python","html","css", True]
+#print (languages)
+#print (languages[2])
+#print(languages[-1])
+#week 2 tupples
+#car = ("toyota", "ford", "bmw")
+#print(car)
+#print(car[2])
+#week 2 dictionary stores values in a key element value
+capital_cities ={
+    "kenya":"nairobi",
+    "uganda":"kampala",
+    "tanzania":"dodoma"
+}
+print(capital_cities)
+print(capital_cities["uganda"])
+#sets collection of unique data dont duplicate items on set
+employee_ids = {1,2,5,8,}
+print (employee_ids)
+
+marks = int(input("Enter your marks: "))
+if marks < 0 or marks > 100:
+ print("invalid marks")
+if marks <= 20:
+ print("grade =E")
+elif marks <= 40:
+ print("grade =D")
+elif marks <= 60:
+ print("grade =C")
+elif marks <= 80:
+ print("grade =B")
+else:
+ print("grade =A")
---
+++ mosh.py
@@ -0,0 +1,11 @@
+#Name='JOHN'
+#age = 20
+#new_patient =True
+#print(Name)
+#print(age)
+#name = input('what is your name? ')
+#Favourite_color = input ('what is your favourite colour')
+#print(input)
+birth_year=input('birth year:')
+age = 2025 - int(birth_year)
+print(age)
